6642b9dd8d15c2e7e6f724b8d79b8aa45948aef3
Shwetha Radhakrishna <shwetha.manvinkurke@gmail.com>
2021-07-29 16:44:53 -0500

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
-require 'simplecov_json_formatter'
-
-SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+# require 'simplecov_json_formatter'
+#
+# SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 
 SimpleCov.start
 
